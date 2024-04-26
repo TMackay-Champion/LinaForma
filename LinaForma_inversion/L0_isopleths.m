f413b5d8005ba88b7ce7a674b5dafa6f07733a37
tober <tobermory2709@btinternet.com>
2024-04-26 10:59:33 +0100

    updated the figures

--- LinaForma_inversion/L0_isopleths.m
+++ LinaForma_inversion/L0_isopleths.m
@@ -7,8 +7,16 @@ model = 'inputs/forward_model.csv';
 observations = 'inputs/observations.csv';
 
 % parameters
-all = 0; % Do you want to plot all of the variables? 1 = YES, 0 = NO
-columns = [1,2,3,4]; % List the columns you are interested in. Will only be selected if all = 0.
+% PLOT 1 = percentage overlap plot
+all1 = 1; % Do you want to plot all of the variables? 1 = YES, 0 = NO.
+columns1 = [1,2,3,4]; % List the columns you want to plot. Only relevant if all = 0.
+
+% PLOT 2 = individual isopleths
+all2 = 0; % Do you want to plot all of the variables? 1 = YES, 0 = NO.
+columns2 = [3,4,5,6]; % List the columns you want to plot. Only relevant if all = 0.
+
+% PLOT 3 will use the contours from Plot2 and the max. percentage from Plot
+% 1
 
 
 %%%%%%%%%%%%%%%%%%%%% CODE %%%%%%%%%%%%%%%%%%%%
@@ -16,27 +24,27 @@ columns = [1,2,3,4]; % List the columns you are interested in. Will only be sele
 
 % Read in data
 input = readtable(model,'VariableNamingRule','preserve');
-contour_data = readmatrix(model);
-probe_data = readmatrix(observations);
+forward_model = readmatrix(model);
+observations = readmatrix(observations);
 
 % Create P-T grid
-temperature = contour_data(:,1);
+temperature = forward_model(:,1);
 T = unique(temperature); T = T(~isnan(T)); ix = length(T);
-pressure = contour_data(:,2);
+pressure = forward_model(:,2);
 P = unique(pressure); P = P(~isnan(P)); iy = length(P);
 [X,Y] = meshgrid(T,P);
 
 
 % Remove T and P columns from contour_data and inputs
-contour_data = contour_data(:,3:end);
+forward_model = forward_model(:,3:end);
 input = input(:,3:end);
 variables_all = input.Properties.VariableNames;
 
 
-% Find which model areas fit the probe data
-for i = 1:size(contour_data,2)
-    model = contour_data(:,i);
-    observed = probe_data(:,i);
+% Find which model areas fit the observations
+for i = 1:size(forward_model,2)
+    model = forward_model(:,i);
+    observed = observations(:,i);
     model_fit = zeros([length(temperature) 1]);
 
     if sum(isnan(observed)) > 0
@@ -49,13 +57,13 @@ for i = 1:size(contour_data,2)
 end
 
 
-% Choosing fields to plot
-if all == 1
+% Choosing fields to include in PLOT 1
+if all1 == 1
 fields = model_comparison;
 variables = variables_all;
 else
-fields = model_comparison(:,columns);
-variables = variables_all(:,columns);
+fields = model_comparison(:,columns1);
+variables = variables_all(:,columns1);
 end
 
 % Field overlap combinations
@@ -87,8 +95,10 @@ percentage_field = sum(fields,2)/size(fields,2)*100;
 maxT = temperature(percentage_field == max(percentage_field));
 maxP = pressure(percentage_field == max(percentage_field));
 fig1 = figure(1);
+set(fig1,'Units','centimeters')
+set(fig1,'Position',[0 0 0.9*21 0.9*21])
 p_field = reshape(percentage_field,[ix iy])';
-pcolor(X,Y,p_field); shading flat; colorbar; hold on
+pcolor(X,Y,p_field); shading flat; c = colorbar; c.Label.String = 'Percentage'; hold on
 xlabel('Temperature (°C)')
 ylabel('Pressure (bars)')
 n = size(fields,2);
@@ -96,14 +106,28 @@ sole_field = 1/n *100;
 st = append('n compositional fields = ',string(n),'  |   1 field = ', ...
     string(sole_field),'%   |   Max = ',string(max(percentage_field)),'% (i.e., ', ...
     string(max(sum(fields,2))),' fields)');
-title('% of fields in a given area')
+title('Percentage of fields in a given area')
 subtitle(st)
 axis square
+sTable = array2table(overlap_variables,'RowNames',variables,'VariableNames',variables);
+
+
+% Choosing fields to include in PLOT 2
+if all2 == 1
+fields = model_comparison;
+variables = variables_all;
+else
+fields = model_comparison(:,columns2);
+variables = variables_all(:,columns2);
+end
+
 
 % Plotting fields
 map = Functions_NO_EDIT.linspecer(size(fields,2));
 loop = -1;
 fig2 = figure(2);
+set(fig2,'Units','centimeters')
+set(fig2,'Position',[0 0 0.9*21 0.9*21])
 for i = 1:size(fields,2)
     loop = loop + 1;
     field = reshape(fields(:,i),[ix iy]);
@@ -114,27 +138,34 @@ for i = 1:size(fields,2)
     p1.FaceAlpha = 0.4;
 end
 legend(variables)
+axis square
 xlabel('Temperature (°C)')
 ylabel('Pressure (bars)')
+title('Isopleth fields')
 
-sTable = array2table(overlap_variables,'RowNames',variables,'VariableNames',variables);
 
-% Plotting max overlap area
+% Choosing fields to include in PLOT 3
+if all2 == 1
+contour_fields = forward_model;
+fields = model_comparison;
+variables = variables_all;
+else
+contour_fields = forward_model(:,columns2);
+fields = model_comparison(:,columns2);
+variables = variables_all(:,columns2);
+end
+
+
+% PLOT 3
 fig3 = figure(3);
+set(fig3,'Units','centimeters')
+set(fig3,'Position',[0 0 0.9*21 0.9*21])
 overlap = sum(fields,2);
 max_overlap = max(overlap);
 overlap(overlap ~= max_overlap) = NaN;
 overlap = reshape(overlap,[ix iy]);
 overlap = overlap';
 p1 = pcolor(X,Y,overlap); shading flat; hold on;
-
-if all == 1
-contour_fields = contour_data;
-variables = variables_all;
-else
-contour_fields = contour_data(:,columns);
-variables = variables_all(:,columns);
-end
 
 map =  Functions_NO_EDIT.linspecer(size(contour_fields,2));
 for iii = 1:size(contour_fields,2)
@@ -146,13 +177,15 @@ for iii = 1:size(contour_fields,2)
 end
 leg = [{'Overlap area'},variables(:)'];
 legend(leg)
+axis square
 xlabel('Temperature (°C)')
 ylabel('Pressure (bars)')
-title('Contour plot, with peak field')
+title('Contour plot with overlap')
 
 % Save table and figures
 save("output_variables/percentage_overlap.mat",'X','Y','p_field');
 saveas(fig1,"FIGURES/percentage_overlap.pdf");
-saveas(fig2,'FIGURES/overlapping_fields.jpeg');
+saveas(fig2,'FIGURES/overlapping_fields.pdf');
+saveas(fig3,'FIGURES/contours_overlap.pdf');
 disp('FINISHED')
 
